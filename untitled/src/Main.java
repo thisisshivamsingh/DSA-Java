63c0e360e5e28c9b02f2d36ccc3eef4f772b2c3f
Shivam <thisisshivam06@gmail.com>
2022-08-15 11:40:21 +0530

    first commit

---
+++ untitled/src/Main.java
@@ -0,0 +1,82 @@
+public class Main {
+    public static void main(String[] args) {
+
+//        System.out.println("Hello World");
+//        System.out.print("Hello World\n");
+//        System.out.print("Hello World");
+
+//        System.out.println("*");
+//        System.out.println("**");
+//        System.out.println("***");
+//        System.out.println("****");
+//        System.out.println("*****");
+
+//        System.out.print("*\n**\n***\n****\n*****\n");
+
+//        System.out.print("*****\n   * \n  *  \n *   \n*****");
+
+//        System.out.println("*****");
+//        System.out.println("   *");
+//        System.out.println("  *");
+//        System.out.println(" *");
+//        System.out.println("*****");
+
+//        int x = 15;
+//        int y = 10;
+//        int sum = x + y;
+//        System.out.println(sum);
+//        System.out.println("Sum of "+ x +" and "+ y +" is "+ sum);
+
+        //xy
+//        int prd = x * y;
+//        System.out.println("Product of "+ x +" and "+ y +" is "+ prd);
+        // x / y, y / x, x % y
+//        int v1 = x / y;
+//        int v2 = y / x;
+//        int v3 = x % y;
+//        System.out.println(v1);
+//        System.out.println(v2);
+//        System.out.println(v3);
+
+        // exp
+//        int exp =  x * y  /  x + y ;
+//        int exp = ( x * y ) / ( x + y );
+//        System.out.println(exp);
+
+//        int x = 11;
+//        if(x%2==0){
+//            System.out.println(x + " is even.");
+//        }else{
+//            System.out.println(x + " is odd.");
+//        }
+//        System.out.println("Hardwork is better than smart work");
+//        int n1 = 20;
+//        int n2 = 30;
+
+//        if(n1==n2){
+//            System.out.println(n1 + " is equal to " + n2);
+//        }else{
+//            if(n1>n2){
+//                System.out.println(n1 + " is greater than " + n2);
+//            }else{
+//                System.out.println(n1 + " is smaller than " + n2);
+//            }
+//        }
+
+//        if(n1==n2){
+//            System.out.println(n1 + " is equal to " + n2);
+//        }else if(n1>n2){
+//            System.out.println(n1 + " is greater than " + n2);
+//        }else{
+//            System.out.println(n1 + " is smaller than " + n2);
+//        }
+
+        int marks = 20;
+
+        if(marks>90){
+            System.out.println("excellent");
+        }else if(marks>80&&marks<=90){
+            System.out.println("hello");
+        }
+    }
+}
