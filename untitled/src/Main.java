8ec1508bdd755e2c6da781c7abddd9cd70b28ddc
Shivam <thisisshivam06@gmail.com>
2022-08-16 20:35:26 +0530

    Reverse a Number

--- untitled/src/Main.java
+++ untitled/src/Main.java
@@ -254,22 +254,31 @@ public class Main {
 //        Scanner scn = new Scanner(System.in);
 //        int n = scn.nextInt();
 
+//        Scanner scn = new Scanner(System.in);
+//        int n = scn.nextInt();
+//
+//        int nod = 0;
+//        int temp = n;
+//        while (temp!=0){
+//            temp=temp/10;
+//            nod ++;
+//        }
+//        int div=(int)Math.pow(10, nod-1);
+//        while(div !=0){
+//            int q = n/div;
+//            System.out.println(q);
+//
+//            n=n%div;
+//            div=div/10;
+//        }
+
         Scanner scn = new Scanner(System.in);
         int n = scn.nextInt();
 
-        int nod = 0;
-        int temp = n;
-        while (temp!=0){
-            temp=temp/10;
-            nod ++;
-        }
-        int div=(int)Math.pow(10, nod-1);
-        while(div !=0){
-            int q = n/div;
-            System.out.println(q);
-
-            n=n%div;
-            div=div/10;
+        while(n>0){
+            int dig = n % 10;
+            n=n/10;
+            System.out.println(dig);
         }
     }
         }
