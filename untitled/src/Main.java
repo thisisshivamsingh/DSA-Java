17082e81dadd891178a58521c20f1f9bde649ec5
Shivam <thisisshivam06@gmail.com>
2022-08-18 19:57:45 +0530

    Pattern-6

--- untitled/src/Main.java
+++ untitled/src/Main.java
@@ -545,6 +545,30 @@ public class Main {
 //            System.out.println();
 //        }
 
+//        Scanner scn = new Scanner(System.in);
+//        int n = scn.nextInt();
+//
+//        int st = n/2+1;
+//        int sp = 1;
+//        for (int i=1; i<=n;i++){
+//            for(int j =1; j<=st;j++){
+//                System.out.print("*\t");
+//            }
+//            for(int j=1;j<=sp;j++){
+//                System.out.print("\t");
+//            }
+//            for(int j=1;j<=st;j++){
+//                System.out.print("*\t");
+//            }
+//            if(i<=n/2){
+//                st--;
+//                sp+=2;
+//            }else{
+//                st++;
+//                sp-=2;
+//            }
+//            System.out.println();
+//        }
     }
         }
 
