1d3347dad6d8734b7435e3de59c5178006061696
Shivam <thisisshivam06@gmail.com>
2022-08-19 15:13:19 +0530

    Pattern 6

---
+++ untitled/src/Pattern6.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class Pattern6 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        int i = 1;
+
+        while(i<=n){
+            int j = 1;
+            int startingNumber = i;
+            while(j<=i){
+            System.out.print(startingNumber);
+            startingNumber++;
+            j++;
+            }
+            System.out.println();
+            i++;
+        }
+    }
+}
