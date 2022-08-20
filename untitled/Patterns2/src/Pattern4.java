437b0b87d4b6878a4683d7819c22adccb8f358ca
Shivam <thisisshivam06@gmail.com>
2022-08-20 19:52:10 +0530

    Pattern 4

---
+++ untitled/Patterns2/src/Pattern4.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Pattern4 {
+    public static void main(String[] args){
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+
+        int dn = n;
+        int i = 1;
+        while(i<=n){
+            int j = 1;
+            while(j<=n-i+1){
+                System.out.print(dn);
+                j++;
+            }
+            System.out.println();
+            i++;
+            dn--;
+        }
+    }
+}
