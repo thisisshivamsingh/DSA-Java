84464c212e474b6c74e13e308fcfe4169f978b58
Shivam <thisisshivam06@gmail.com>
2022-08-20 15:40:35 +0530

    Pattern 10 and Pattern 11

---
+++ untitled/src/Pattern11.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class Pattern11 {
+
+
+    public static void main(String[] args) {
+
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        int i = 1;
+
+        while(i<=n){
+            int j = 1;
+            char startingChar = (char)('A'+i-1);
+            while(j<=i){
+                System.out.print(startingChar);
+                startingChar=(char)(startingChar+1);
+                j++;
+            }
+            System.out.println();
+            i++;
+        }
+    }
+
+}
+
