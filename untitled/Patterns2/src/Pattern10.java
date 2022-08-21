9dc1cede2cb6a04269f14c64be30c34f390c43bd
Shivam <thisisshivam06@gmail.com>
2022-08-21 17:36:02 +0530

    Pattern 10

---
+++ untitled/Patterns2/src/Pattern10.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class Pattern10 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+
+        int i = 1;
+        while(i<=n){
+            int spaces = 1;
+            while(spaces<=i-1){
+                System.out.print(" ");
+                spaces++;
+            }
+            int j = 1;
+            while(j<=n){
+                System.out.print("*");
+                j++;
+            }
+            System.out.println();
+            i++;
+        }
+    }
+}
