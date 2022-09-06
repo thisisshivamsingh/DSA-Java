8cf5b1452dca41e1932058af7ec1be9e7087119a
Shivam <thisisshivam06@gmail.com>
2022-09-07 00:43:09 +0530

    Binary Search Algorithm

---
+++ Array/src/binarySearchAlgorithm.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class binarySearchAlgorithm {
+    public static void main(String[] args) {
+        // write your code here
+        int [] arr = {10, 20, 30, 40, 50, 60, 70, 80, 90, 100};
+        int data = 20;
+
+        int l = 0;
+        int h = arr.length - 1;
+        while(l <= h){
+            int m = (l + h) / 2;
+            if(data > arr[m]){
+                l = m + 1;
+            } else if (data < arr[m]) {
+                h = m - 1;
+            }else{
+                System.out.println(m);
+                return;
+            }
+        }
+        System.out.println(-1);
+    }
+}
